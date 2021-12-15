af3aaa4efbc092635a3b6a2df8b6ca0ca0194237
Gauri Pethkar <gauri.pethkar7@gmail.com>
2021-12-15 23:01:35 +0530

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,15 +50,3 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
-//Contact Form
-const inputs = document.getElementsByClassName('form-control');
-
-for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("focus", function (event){
-    this.nextElementSibling.classList.add("active")
-  }); 
-  inputs[i].addEventListener("focusout", function(event) {
-  if (this.value === "") {
-   this.nextElementSibling.classList.remove("active");
-  }})
-}
