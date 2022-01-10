7ee762e0f141317ffa62370501cde12a5679c6c8
Gauri Pethkar <gauri.pethkar7@gmail.com>
2022-01-10 15:45:31 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "flex";
     dots[slideIndex-1].className += " active";
 }
 
